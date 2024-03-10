1bbc2f965f7e6c0a3be8cfe9dc7b5d39f01152dd
Mohammedsabeel063 <162916472+Mohammedsabeel063@users.noreply.github.com>
2024-03-11 01:09:11 +0530

    Create firstproj.java

---
+++ firstproj.java
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My First HTML Project</title>
+  </head>
+  <body>
+    <h1>Welcome to my first HTML project!</h1>
+    <p>This is a paragraph of text.</p>
+  </body>
+</html>
